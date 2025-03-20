e271c417606a80b3d2bd6b2dd510f7e254764271
n-pontes <nuno06.np@gmail.com>
2025-03-20 10:56:27 +0100

    Add unique ID generator - Generates an unique ID with crypto.randomUUID() - Ensures each book has a unique and stable identifier, preventing issues when books are removed or rearranged

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const myBooks = [];
 // The book constructor
 class Book {
     constructor (title, author, genre, pages, year, read) {
+        this.id = crypto.randomUUID(); // Generates an unique ID
         this.title = title;
         this.author = author;
         this.genre = genre;
@@ -16,7 +17,10 @@ class Book {
 // Function that adds a new book into the myBooks array
 const addBookToLibrary = (title, author, genre, pages, year, read) => {
     const newBook = new Book(title, author, genre, pages, year, read);
+    crypto.randomUUID(newBook); 
     myBooks.push(newBook);
 }
 
+addBookToLibrary('The Count of Monte Cristo', 'Alexandre Dumas', 'Historical Fiction', 1276, 'January 15, 1846', 'Read');
+console.table(myBooks);
 
