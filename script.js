78cf5a9e6ae8a4ed90e3abb9970ce68b7f55e87b
n-pontes <nuno06.np@gmail.com>
2025-03-20 09:59:39 +0100

    Add array to save book and book constructor - Added empty array myLibrary to hold new books that the constructor creates - Added object constructor logic, will later serve as the details to be filled in a form, for every new book the user creates

---
+++ script.js
@@ -0,0 +1,9 @@
+// Books will be stored in this array
+const myLibrary = [];
+
+// The book constructor
+class Book {
+    constructor (tittle, author, genre, pages, year, read) {
+        // code will go here later
+    }
+}
